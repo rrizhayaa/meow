d3ae097e89e2ac7e46b91fbe64e3ae6a14389bc5
rrizhayaa <118274077+rrizhayaa@users.noreply.github.com>
2022-12-13 14:54:35 +0300

    Add files via upload

--- Aviary.py
+++ Aviary.py
@@ -1,28 +1,76 @@
 class Aviary:
-    def __init__(self,name,biome,square):
-        self.__name = name
-        self.__biome = biome
-        self.__square = square
-        self.howMuchFood = 0
-        self.food = "Рыба", "Мясо","Ягоды","Семена","Трава"
+
+    def __init__(self, Name, Biome, Square):
+        self.__name = Name
+        self.__biome = Biome
+        self.__square = Square
+        self.__animals = []
+        self.__food = "Рыба", "Мясо", "Ягоды", "Трава", "Семена"
+        self.__amountOfFood = 100
 
     @property
-    def Biome(self):
+    def name(self):
+        return self.__name
+
+    @property
+    def biome(self):
         return self.__biome
 
     @property
-    def Square(self):
+    def square(self):
         return self.__square
-    
+
     @property
-    def NameAviary(self):
-        return self.__name
+    def animals(self):
+        return self.__animals
 
-    def PourFood(self):
-        if self.howMuchFood >= 0 and self.howMuchFood <=200:
-            self.howMuchFood+=100
-            print("В", self.__name, "насыпали еду")
+    @property
+    def food(self):
+        return self.__food
 
-    def HowMuchFood(self):
+    def addAnimal(self, Animal):
+        if Animal.aviary:
+            return print (Animal, "уже находится в вольере", self.__name)
+
+        if not(Animal.biome == self.__biome):
+            return print("Вольер", self.__name, "не подходит для животного:", Animal.type)
+
+        if not(self.__square >= Animal.square):
+            return print("В вольере", self.__name, "не хватает места для животного", Animal.type)
+
+        if (not(Animal.isVegan) and (self.__animals[0].isVegan)):
+            return print("Животное", Animal.type, "не сочетается с животными в вольере", self.__name)
+
+        if (Animal.sigh = "Хищник") and not(self.__animals[0].sigh = "Хищник"):
+            return print("Животное", Animal.type, "не сочетается с животными в вольере", self.__name)
+
+        self.__animals.append(Animal)
+        self.__square -= Animal.square
+        Animal.aviary = self
+        return print(Animal.name, "добавлен в вольер", self.__name)
+
+    def removeAnimal(self, Animal):
+        if not(Animal.aviary == self):
+            return print(Animal.name, "не находится в вольере", self.__name)
+
+        self.__animals.remove(Animal)
+        Animal.aviary = 0
+        self.__square += Animal.square
+        return print(Animal.name, "больше не находится в вольере", self.__name)
+
+    def doSound(self):
+        if len(self.__animals)==0:
+            return print("В вольере", self.__name, "нет животных, чтобы издать звук")
+
+        for Animal in self.__animals:
+            print(Animal.name, "издает звук", Animal.doSound)
+
+    def feedAnimals(self,value):
+        if self.__amountOfFood > 200:
+            self.__amountOfFood = 200
+        elif self.__amountOfFood < 0:
+            self.__amountOfFood = 0
+        else:
+            self.__amountOfFood = value
 
 
